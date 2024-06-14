acdd97f0949a0b33cc9729db7ef0277e0a840008
Antonio Henrique Nascimento - A <henriquecvi22@gmail.com>
2024-06-14 01:36:53 +0000

    adicionada a funcao de jogar novamente e finalizacao do projeto

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -3,10 +3,13 @@ import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
+const botaoJogarNovamente = document.querySelector("main button")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
+
+botaoJogarNovamente.addEventListener("click", jogarNovamente)
 
 verificarTema(body, botaoTema)
 
@@ -21,18 +24,11 @@ function alterarAssunto() {
     assuntoTitulo.innerText = assunto
 }
 
-alterarAssunto()
+function jogarNovamente() {
+    localStorage.removeItem("pontos")
+    localStorage.removeItem("assunto")
 
-function inserirResultado() {
-    const sectionPontuacao = document.querySelector(".pontuacao")
-    const divAssunto = document.querySelector(".assunto")
-    const pontos = localStorage.getItem("pontos")
-
-    sectionPontuacao.innerHTML = `
-        ${divAssunto.outerHTML}
-        <strong>${pontos}</strong>
-        <p>de 10</p>
-    `
+    window.location.href = "../../index.html"
 }
 
 inserirResultado()
